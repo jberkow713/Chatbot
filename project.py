6e021cfbd1db360c8e19d0dd74430a73e35e23a9
Jesse <jberkow@brandeis.edu>
2024-10-15 13:47:24 -0400

    vector store addition

--- project.py
+++ project.py
@@ -11,18 +11,21 @@ from langchain_core.documents import Document
 import chromadb
 from langchain_openai import OpenAIEmbeddings
 
-
-# Create a prompt template for the user, to ask about websites
-# Take the top few sites that the model returns, and throw them into the web scraper
-# From there, chunk the textual data, and send it to the vector store/db
+# TODO
 # Then take the users secondary query, and compare it to the information in the data store, 
 # Return information to the user
-
 # Ask if user has any more questions , repeat until user says no
+
+
+COUNT = 0
 
 load_dotenv()
 model = ChatOpenAI(model='gpt-4o')
-chat history = []
+
+embeddings = OpenAIEmbeddings(model='text-embedding-3-small')
+vector_store = Chroma(collection_name = 'test_collection',
+            embedding_function=embeddings,
+            )
 
 def find_website(website):
     '''
@@ -45,92 +48,67 @@ def find_website(website):
             websites.append(w[1:].split(']')[0])
     return websites
 
-format_prompt = RunnableLambda(lambda x: prompt_template.format_prompt(**x))
-invoke_model = RunnableLambda(lambda x: model.invoke(x.to_messages()))
-parse_output = RunnableLambda(lambda x: x.content)
-
-chain = RunnableSequence(format_prompt,invoke_model,parse_output)
-
-response = chain.invoke({"topic":"sharks", "joke_count":2})
-
-def scrape(website):
+def scrape(site):
     
-    loader = WebBaseLoader(url)
+    loader = WebBaseLoader([site])
     documents = loader.load()
-
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    text_splitter = CharacterTextSplitter(separator="\n",chunk_size=1000, chunk_overlap=0)
     docs = text_splitter.split_documents(documents)
+    Words = []
     for doc in docs:    
         words = doc.page_content.split()
-        print(' '.join(words))
+        Words.append(' '.join(words))
+    return Words    
 
-while True:
-    query = input("What website would you like to learn about today?: ")
+def add_store(text,Id):
+    doc= [Document(
+    text,
+    id=Id)]
+
+    vector_store.add_documents(doc)
+    return 
+
+def add_store_site(site):
     
-    if query.lower() == 'exit':
-        break
+    # This will iterate through a link and add information in chunks to the vector store 
+    scraped = scrape(site)
+    global COUNT 
+    for x in scraped:
+        add_store(x,COUNT)
+        COUNT+=1
+    return     
 
-    website = model.invoke(HumanMessage(content=query))
-    response= website.content
+add_store_site('https://www.apple.com')
 
-    chat_history.append(AIMessage(content=response))
+    # retriever = vector_store.as_retriever(
+    #     search_type="similarity_score_threshold",
+    #     search_kwargs={'k':2, "score_threshold":.3}
 
-    print(response)
-
-
-prompt_template = ChatPromptTemplate.from_messages([
-    ("system","Which website would you like to get information from?"),
-    ("human","Tell me {joke_count} jokes")
-])
-
-format_prompt = RunnableLambda(lambda x: prompt_template.format_prompt(**x))
-invoke_model = RunnableLambda(lambda x: model.invoke(x.to_messages()))
-parse_output = RunnableLambda(lambda x: x.content)
-
-chain = RunnableSequence(format_prompt,invoke_model,parse_output)
-
-response = chain.invoke({"topic":"sharks", "joke_count":2})
-
-print(response)
+    # )
+    # query = "This is a document about hawaii"
+    # relevant_docs = retriever.invoke(query)
+    # if relevant_docs!=[]:
+    #     for x in relevant_docs:
+    #         print(x.page_content)
 
 
-
-url = ['https://www.apple.com']
-loader = WebBaseLoader(url)
-documents = loader.load()
-
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-docs = text_splitter.split_documents(documents)
-for doc in docs[:3]:
+# while True:
+#     query = input("What website would you like to learn about today?: ")
     
-    words = doc.page_content.split()
-    print(' '.join(words))
+#     if query.lower() == 'exit':
+#         break
 
-embeddings = OpenAIEmbeddings(model='text-embedding-3-small')
-vector_store = Chroma(collection_name = 'test_collection',
-            embedding_function=embeddings,
-            persist_directory="./chroma_langchain_db")
+#     websites = find_website(query)
+#     for s in websites:
+      
 
-query = "User Query Goes here, related to website"
-relevant_docs = retriever.invoke(query)
-if relevant_docs!=[]:
-    for x in relevant_docs:
-        print(x.page_content) 
+# query = "User Query Goes here, related to website"
+# relevant_docs = retriever.invoke(query)
+# if relevant_docs!=[]:
+#     for x in relevant_docs:
+#         print(x.page_content) 
 
-chat_history = []
-system_message = SystemMessage(content="You are a very sarcastic AI assistant")
-chat_history.append(system_message)
-
-while True:
-    query = input("You: ")
-    
-    if query.lower() == 'exit':
-        break 
-    chat_history.append(HumanMessage(content=query))
-
-    result = model.invoke(chat_history)
-    response= result.content 
-    chat_history.append(AIMessage(content=response))
-
-    print(response)
+# chat_history = []
+# system_message = SystemMessage(content="You are a very sarcastic AI assistant")
+# chat_history.append(system_message)
 
